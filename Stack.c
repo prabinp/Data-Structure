87d77d0574db68c5e74d1ff4e669e123d3b3c846
Prabin Pramanik <thisisprabin@users.noreply.github.com>
2016-08-09 01:42:31 +0530

    Update Stack.c

--- Stack.c
+++ Stack.c
@@ -2,7 +2,6 @@
  * Normal operation of stack like push pop display etc.
  */
  
- 
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -74,12 +73,60 @@ int pop(Stack *stack){
 	}
 }
 
+Stack *reverseStack(Stack *stack){
+	int i;
+	
+	if(isEmpty(stack) == 1){	
+		printf("\nStack is empty\n");
+		return;
+	} else {
+		Stack *arr = init(stack, stack->top);
+		for(i=0; i<=stack->top; i++){
+			push(arr, stack->a[i]);
+		}
+	
+		for(i=0; i<=stack->top; i++){
+			stack->a[i] = pop(arr);
+		}
+	}
+	
+	return stack;
+}
+
+int secondBigElement(Stack *stack){
+	
+	int i, j, b;
+	int A[stack->top];
+
+	for(i=0; i<=stack->top; i++){
+		A[i] = stack->a[i];
+	}
+	
+	for(i=0; i<=stack->top; i++) {
+		for(j=i; j<=stack->top; j++) {
+			if(A[i] < A[j]) {
+				b = A[j];
+				A[j] = A[i];
+				A[i] = b ;
+			}
+		}
+	}
+	
+	return A[1];
+}
+
+/*
+Stack *balancedParentheses(Stack *stack){	 
+}
+*/
+
 void printStack(Stack *stack){
 	int i;
 	for(i=stack->top; i>=0; i--){
 		printf("%d\t", stack->a[i]);
 	}
 }
+
 
 int main(){
 	
@@ -99,6 +146,8 @@ int main(){
 		printf("\n2 - display");
 		printf("\n3 - check the peek");
 		printf("\n4 - pop the elements");
+		printf("\n5 - reverse the stack elements");
+		printf("\n6 - find the second big element");
 		
 		printf("\n\nEnter the option - ");
 		scanf("%d", &ch);
@@ -112,13 +161,18 @@ int main(){
 			case 2: printStack(stack);
 			break;
 			
-			case 3: value = peek(stack);
-					printf("\n%d\n", value);
+			case 3: printf("\n%d\n", peek(stack));
 			break;
 			
-			case 4: value = pop(stack);
-					printf("\n%d\n", value);
+			case 4: printf("\n%d\n", pop(stack));
 			break;		
+			
+			case 5: reverseStack(stack);
+					printStack(stack);
+			break;
+			
+			case 6: printf("\n%d\n", secondBigElement(stack));
+			break;
 			
 			default: printf("\nERROR: Wrong input\n");
 			break; 
